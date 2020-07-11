38d5c90a761f2082e0cb021a14b0a40d7b06b806
Frank Zhao <fzhao03@gmail.com>
2020-07-11 17:06:49 -0400

    redirect to new_tweet_path after tween form submission

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,14 @@ class TweetsController < ApplicationController
 		
 	end
 
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+
+	end
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
+
 end
